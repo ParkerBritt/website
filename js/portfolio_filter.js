acee8f49a061de65f8f6705e37cf98035a7d4c46
parker <parker@parkerbritt.com>
2024-09-27 00:04:40 +0100

    rewrite filter system for single search tag, removed extra tags, fine tune animations

--- js/portfolio_filter.js
+++ js/portfolio_filter.js
@@ -1,36 +1,67 @@
-let filterHoudini = false;
-let filterCommercial = false;
-let filterProgramming = false;
-let filterPersonal = false;
+// let filterHoudini = false;
+// let filterCommercial = false;
+// let filterProgramming = false;
+// let filterPersonal = false;
+// let filterAll = false;
+
+let filterButtonIds = ["houdini_button", "programming_button", "all_button"];
+
+setFilterButtonState(document.getElementById('all_button'), true);
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function setFilterButtonState(button, state){
+    if(state){
+        // set value
+        button.value = "on"; 
+
+        // add style
+        button.classList.add("active")
+
+        // reset buttons
+        for(let i=0; i<filterButtonIds.length; i++){
+            let curId = filterButtonIds[i];
+            if(curId!=button.id){
+                setFilterButtonState(document.getElementById(curId), false);
+            }
+        }
+    } else{
+        // remove style
+        button.classList.remove("active")
+        // set value
+        button.value = "off";
+    }
+}
+function getFilterButtonState(button){
+    return button=="on";
+}
+function toggleFilterButton(button){
+    button.value = !getFilterButtonState(button);
+}
 
 function onFilterButtonClicked(id, tag) {
     const button = document.getElementById(id);
-    button.classList.toggle("filter-button-clicked")
-    if(tag==='houdini'){
-        filterHoudini = !filterHoudini;
-    } else if(tag==='commercial'){
-        filterCommercial = !filterCommercial;
-    } else if (tag==='personal'){
-        filterPersonal = !filterPersonal;
-    } else if (tag==='programming'){
-        filterProgramming = !filterProgramming;
-    }
-    console.log("toggled: " + filterHoudini, filterProgramming, filterCommercial, filterPersonal);
-    filterPortfolioItems();
+    setFilterButtonState(button, true);
+    filterPortfolioItems(tag);
 }
-function filterPortfolioItems(){
+async function filterPortfolioItems(activeTag){
     const portfolioItems = document.querySelectorAll("[data-portfolio-item-tag]")
-    console.log("len:" + portfolioItems.length);
+
     let displayedCounter = 0;
+    for (let i=0; i<portfolioItems.length; i++){
+        const portfolioItem = portfolioItems[i];
+        portfolioItem.style.animationDelay = (0)+'s';
+        portfolioItem.classList.remove("active");
+    }
+    await sleep(150);
     for (let i=0; i<portfolioItems.length; i++){
         const portfolioItem = portfolioItems[i];
         const itemTags = JSON.parse(portfolioItem.getAttribute('data-portfolio-item-tag'));
         console.log("portfolio item: " + portfolioItem.innerText + ' ' + JSON.parse(portfolioItem.getAttribute('data-portfolio-item-tag')));
         if(
-            (filterHoudini && itemTags.includes('houdini')) ||
-            (filterProgramming && itemTags.includes('programming')) ||
-            (filterCommercial && itemTags.includes('commercial')) ||
-            (filterPersonal && itemTags.includes('personal'))
+            activeTag==="all" || itemTags.includes(activeTag)
         ){
             if(!portfolioItem.classList.contains("active")){
                 displayedCounter++;
@@ -41,10 +72,11 @@ function filterPortfolioItems(){
 
             const projectName = portfolioItem.getAttribute("data-project-name");
             portfolioItem.style.setProperty("--portfolio-item-name", '\"'+projectName+'\"');
-        } else {
-            portfolioItem.style.animationDelay = (0)+'s';
-            portfolioItem.classList.remove("active");
         }
+        // } else {
+        //     portfolioItem.style.animationDelay = (0)+'s';
+        //     portfolioItem.classList.remove("active");
+        // }
 
     }
 }
