be2fddb77249403a8b564c8a4d086467a456b49a
Parker Britt <parker@parkerbritt.com>
2025-01-30 02:28:49 +0000

    refactor: cleanup, fix start script

--- start-dev.sh
+++ start-dev.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-podman-compose -f ./docker-compose-dev.yaml up -d --force-recreate
-browser-sync start --proxy "0.0.0.0:8000" --files "./**/*" --no-ui --no-notify
+podman-compose -f ./docker-compose-prod.yaml up -d
+browser-sync start --proxy "0.0.0.0:8001" --files "./**/*" --no-ui --no-notify
