11909c32254aefd5e35700ccccd9aa8f355a54f0
parker <parker@parkerbritt.com>
2025-09-20 16:12:58 +0100

    feat: add katex and article figure component

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,18 @@ import react from '@astrojs/react';
 
 import mdx from '@astrojs/mdx';
 
+import rehypeKatex from 'rehype-katex';
+import remarkMath from 'remark-math';
+
 // https://astro.build/config
 export default defineConfig({
-    integrations: [react(), mdx()],
+    integrations: [
+        react(),
+        mdx({
+            remarkPlugins: [remarkMath],
+            rehypePlugins: [rehypeKatex]
+        }),
+    ],
     markdown: {
         shikiConfig: {
             theme: 'dracula',
